798d9092a0e2cd91b0a8ce7b014bbf7f7b745c3c
milandharani26 <113498278+milandharani26@users.noreply.github.com>
2024-12-27 14:56:09 +0530

    remove some logos

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -4,16 +4,15 @@ import './whatGPT3.css';
 
 const WhatGPT3 = () => (
   <div className="gpt3__whatgpt3 section__margin" id="HIW">
-    <div className="gpt3__whatgpt3-feature">
+    {/* <div className="gpt3__whatgpt3-feature">
       <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
-    </div>
-    <div className="gpt3__whatgpt3-heading">
+    </div> */}
+    {/* <div className="gpt3__whatgpt3-heading">
       <h1 className="gradient__text">
-        {/* The possibilities are beyond your imagination  */}
         How It Works
       </h1>
       <p>Explore the Library</p>
-    </div>
+    </div> */}
     <div className="gpt3__whatgpt3-container">
       <Feature title="Face Detection" text="Our system detects and analyzes facial features as employees approach." />
       <Feature title="Matching & Verification" text="Detected faces are matched against the employee database for verification." />
